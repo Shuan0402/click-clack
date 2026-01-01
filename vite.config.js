a7316ae516bc908a60a63903b02d61fc42d899fe
Shuan0402 <st980155@gmail.com>
2026-01-01 15:39:31 +0800

    set pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './click-clack/',
+  // 暫時先註解掉這一行，本地開發會比較順利
+  // base: '/click-clack/', 
 })
